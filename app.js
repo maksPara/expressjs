1cd7ff7c5ad10e445cc77197b7c7323a13247c15
Maks Paradowski <maksparadowski@gmail.com>
2024-08-23 17:37:13 +0200

    add cookies and log them

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = 3055;
 
+app.use(cookieParser());
+
 app.get('/', (req, res) => {
-  res.send('Hello Maks!');
+  console.log(req.cookies);
+  res.send('Hello Maksymilian!');
 });
 
 app.listen(port, () => {
